268b9af4d667c1890af2cf8d5a2dd3a28138c307
Sarah Lim <sarah@sarahlim.com>
2020-09-01 14:35:02 -0700

    Add screen-reader text component

---
+++ src/components/ScreenReaderText.tsx
@@ -0,0 +1,24 @@
+/**
+ * Text only visible to screen readers.
+ */
+import * as React from "react"
+
+type ScreenReaderTextProps = {
+    children: React.ReactNode
+}
+
+export function ScreenReaderText({ children }: ScreenReaderTextProps) {
+    return (
+        <span
+            style={{
+                clip: "rect(0 0 0 0)",
+                overflow: "hidden",
+                position: "absolute",
+                height: 1,
+                width: 1,
+            }}
+        >
+            {children}
+        </span>
+    )
+}
