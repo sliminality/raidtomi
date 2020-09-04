6724aa59ecab074e265f7b2fe7c63ca26d770c13
Sarah Lim <sarah@sarahlim.com>
2020-09-03 23:14:26 -0700

    Implement seed input functionality

--- src/components/Seed.tsx
+++ src/components/Seed.tsx
@@ -8,6 +8,60 @@ type SeedProps = {
     updateValue: React.Dispatch<React.SetStateAction<BigInt | undefined>>
 }
 
-export function Seed(props: SeedProps) {
-    return <div>seed</div>
+export function Seed({ value, updateValue }: SeedProps) {
+    const valueAsHex = React.useMemo(() => (value ? value.toString(16) : ""), [
+        value,
+    ])
+    const [invalidState, setInvalidState] = React.useState<
+        | {
+              temporaryValue: string
+              message?: string
+          }
+        | undefined
+    >()
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const text = e.currentTarget.value
+        if (text === "") {
+            updateValue(undefined)
+            return
+        }
+
+        let bigInt
+        try {
+            bigInt = BigInt(`0x${text}`)
+        } catch (error) {
+            setInvalidState({ temporaryValue: text, message: "Invalid seed" })
+            return
+        }
+        if (bigInt > BigInt("0xFFFFFFFFFFFFFFFF")) {
+            setInvalidState({ temporaryValue: text, message: "Invalid seed" })
+            return
+        }
+
+        updateValue(bigInt)
+        setInvalidState(undefined)
+    }
+
+    const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+        setInvalidState(undefined)
+    }
+
+    return (
+        <div>
+            <label>
+                Seed
+                <input
+                    type="text"
+                    value={
+                        invalidState ? invalidState.temporaryValue : valueAsHex
+                    }
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    placeholder="bb810e6006a2a035"
+                />
+                {invalidState && invalidState.message}
+            </label>
+        </div>
+    )
 }
