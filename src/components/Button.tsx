32b080295f07875688b33c3eecc1bd350033fcde
Sarah Lim <sarah@sarahlim.com>
2020-11-19 19:16:18 -0800

    Update button styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,10 @@
 import * as React from "react"
 import { StyleSheet, css } from "aphrodite/no-important"
 
-export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
-    return <button className={css(styles.button)} {...props} />
+export function Button(
+    props: React.ButtonHTMLAttributes<HTMLButtonElement>,
+): JSX.Element {
+    return <button className={css(styles.button, props.disabled && styles.buttonDisabled)} {...props} />
 }
 
 const styles = StyleSheet.create({
@@ -13,5 +15,12 @@ const styles = StyleSheet.create({
         borderRadius: 4,
         boxShadow: "var(--button-outline)",
         padding: "6px 12px",
+        marginRight: 8,
+        marginBottom: 4,
     },
+    buttonDisabled: {
+        background: "var(--button-disabled)",
+        boxShadow: "var(--button-disabled-outline)",
+        cursor: "not-allowed"
+    }
 })
